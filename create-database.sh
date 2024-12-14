c20ae049c67236c25847634b54a4377c6b4ab1d4
Kaique Motta <kaique.motta@hotmail.com>
2024-12-14 13:40:51 -0300

    feat: comando mysql

--- create-database.sh
+++ create-database.sh
@@ -7,5 +7,12 @@ DB_USER=${DB_USER:-"root"}
 DB_PASS=${DB_PASS:-"password"}
 DB_NAME=${DB_NAME:-"my_database"}
 
+# Separar o host e a porta, caso a porta esteja incluída no DB_HOST
+HOST=$(echo $DB_HOST | cut -d':' -f1)
+PORT=$(echo $DB_HOST | cut -d':' -f2)
+
+# Se não foi fornecida uma porta no DB_HOST, use a porta padrão
+PORT=${PORT:-$DB_PORT}
+
 # Comando para criar o banco de dados
-mysql -h $DB_HOST -P $DB_PORT -u $DB_USER -p$DB_PASS -e "CREATE DATABASE IF NOT EXISTS $DB_NAME;"
+mysql -h $HOST -P $PORT -u $DB_USER -p$DB_PASS -e "CREATE DATABASE IF NOT EXISTS $DB_NAME;"
