cfde0842a96d4590811129451caf2745c6492c7e
Kaique Motta <kaique.motta@hotmail.com>
2024-12-14 16:55:05 -0300

    feat: tentando add file

--- create-database.sh
+++ create-database.sh
@@ -8,15 +8,6 @@ echo "DB_USER = $DB_USER"
 echo "DB_PASS = $DB_PASS"
 echo "DB_NAME = $DB_NAME"
 
-# Variáveis de ambiente com valores padrão
-DB_HOST=${DB_HOST:-"localhost"}
-DB_PORT=${DB_PORT:-3306}
-DB_USER=${DB_USER:-"root"}
-DB_PASS=${DB_PASS:-"password"}
-DB_NAME=${DB_NAME:-"my_database"}
-
-# Imprimir o comando MySQL que será executado
-echo "Executing command: mysql -h $DB_HOST -P $DB_PORT -u $DB_USER -p$DB_PASS -e \"CREATE DATABASE IF NOT EXISTS $DB_NAME;\""
-
 # Comando para criar o banco de dados
+echo "Executing command: mysql -h $DB_HOST -P $DB_PORT -u $DB_USER -p$DB_PASS -e \"CREATE DATABASE IF NOT EXISTS $DB_NAME;\""
 mysql -h $DB_HOST -P $DB_PORT -u $DB_USER -p$DB_PASS -e "CREATE DATABASE IF NOT EXISTS $DB_NAME;"
